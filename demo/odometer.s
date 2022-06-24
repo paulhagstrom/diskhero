595fe05ad154f3add63016a9b0cb5d241cb843e1
Paul Hagstrom <hagstrom@bu.edu>
2022-06-24 06:18:40 +0000

    Wire up HBL interrupts

--- demo/odometer.s
+++ demo/odometer.s
@@ -36,6 +36,8 @@ D_SCROLLON  = $C0D9     ; enable smooth scroll (vertical shift)
 D_SCROLLOFF = $C0D8     ; disable smooth scroll
 
 R_TONEHBL   = $FFE0     ; tone (bits 0-5), I/O count (6), slot nmi (7)    
+RE_T2CL     = $FFE8     ; ffdx timer2 count low byte
+RE_T2CH     = $FFE9     ; ffdx timer2 count high byte
 RD_AUXCTL   = $FFDB     ; ffdx auxiliary control register
 RE_AUXCTL   = $FFEB     ; ffex auxiliary control register
 RD_PERCTL   = $FFDC     ; ffdx peripheral control register
@@ -116,6 +118,10 @@ intreturn:  pla
 ; timer2 interrupt handler
 inttimer:   lda #$20        ;clear timer2 flag
             sta RE_INTFLAG
+            lda #$08        ; for now reset count to 8 lines
+            sta RE_T2CL
+            lda #$00
+            sta RE_T2CH
             dec CountHBL + 3
             bpl timerdo
             lda #$07
@@ -247,86 +253,174 @@ setupenv:   ; save IRQ vector and then install ours
             sta IRQVECT + 1
             lda #>inthandler
             sta IRQVECT + 2
-            ; [0-------] F000.FFFF RAM (1=RAM)
-            ; [-1------] ROM#1 (0=ROM#2)
-            ; [--1-----] True stack ($100) (0=alt stack)
-            ; [---1----] C000.CFFF read-only (1=read/write)
-            ; [----0---] Reset key disabled (1=enabled)
-            ; [-----1--] video enabled (0=disabled)
-            ; [------1-] C000.CFFF I/O (0=RAM)
-            ; [-------1] 2MHz clock (0=1MHz)
-            lda #$77        ; 2MHz, video, I/O, reset, r/w, ram, ROM#1
+            
+            ; bank register - $FFEF - E-VIA input register A
+            ; we will just leave this as-is, which will be the highest
+            ; available bank given the RAM in the machine.
+            
+            ; ZP register - $FFD0 - D-VIA input/output register B
+            ; Convention: user $1A, interrupts $00, SOS $18.
+            ; extended addressing only works from $18-$1F(!)
+            ; We will still try to point it at graphics pages occasionally.
+            ; for now, leave it where it is, presumed to be $1A.
+            
+            ; set environment - $FFDF - D-VIA input register A
+            ; because we will use ZP to draw, we need stack to be true.
+            ;    [0-------] F000.FFFF RAM (1=ROM)
+            ;    [-1------] ROM#1 (0=ROM#2)
+            ;    [--1-----] True stack ($100) (0=alt stack)
+            ;    [---1----] C000.CFFF read-only (1=read/write)
+            ;    [----0---] Reset key disabled (1=enabled)
+            ;    [-----1--] video enabled (0=disabled)
+            ;    [------1-] C000.CFFF I/O (0=RAM)
+            ;    [-------1] 2MHz clock (0=1MHz)
+            lda #%01110111      ; 2MHz, video, I/O, reset, r/w, ram, ROM#1
             sta R_ENVIRON
-            lda #$7F        ; disable & clear all interrupts (MSB=clear, other bits=interrupts)
+            
+            ; D-VIA
+            ; register A: environmental register (out)
+            ; register B: zero page register - and RTC (out)
+            ; CA1 - global slot IRQ
+            ; CA2 - sw1, which I think is the open apple key
+            ; CB1 - serial out, CB2 - serial in.  Usually printer.  Maybe joystick.
+            
+            ; E-VIA
+            ; register A:
+            ; [x-------] Any IRQ (in)
+            ; [-x------] Closed apple key (in)
+            ; [--x-----] Slot 2 IRQ (in)
+            ; [---x----] Slot 1 IRQ (in)
+            ; [-----xxx] Selected bank (out)
+            ; register B:
+            ; [--xxxxxx] Sound generator (out)
+            ; [-x------] I/O count
+            ; [x-------] nmi in a slot
+            ; CA1 - RTC interrupt (neg edge active in)
+            ; CA2 - keyboard interrupt (ind neg edge active) - sets bit 0 of IFR
+            ; CB1, CB2, shift - VBL
+            
+            ; disable & clear all D-VIA interrupts (MSB=clear, other bits=interrupts)
+            ; D-VIA Int - flag $FFDD, enable $FFDE
+            ;     0------- disable
+            ;     -1------ timer 1
+            ;     --1----- timer 2
+            ;     ---1---- CB1
+            ;     ----1--- CB2
+            ;     -----1-- shift register
+            ;     ------1- CA1
+            ;     -------1 CA2
+            lda #%01111111
             sta RD_INTENAB
             sta RD_INTFLAG
-            ; CB2, CA1, shift register handle VBL behavior.
-            ; E-VIA Int Flag
-            ; [0-------] disable
-            ; [-0------] timer 1
-            ; [--0-----] timer 2
-            ; [---0----] CB1
-            ; [----1---] CB2
-            ; [-----1--] shift register
-            ; [------1-] CA1
-            ; [-------0] CA2
-            lda #$0E        ; disable & clear CB2, shift register, CA1
-            sta RE_INTENAB
-            sta RE_INTFLAG
-            ; D-VIA Aux control:
-            ; [---000--] disabled
-            ; other options (maybe copy somewhere else)
-            ; [---000--] disabled
-            ; [---100--] shift out free running at T2 rate
-            ; [---1----] shift out
-            ; [---0----] shift in
-            ; [----01--] under control of T2
-            ; [----10--] under control of +2 (?)
-            ; [----11--] under control of ext clock
-            ; T1 has two latches and a 16 bit counter.  Down to zero -> interrupt
-            ; one shot keeps counting, free-run resets and counts again
-            ; T2 can count PB6 negatives, or run in one shot mode.
-            ; Count: load number to count into T2, dec on pulses, interrupt at zero, counting continues
-            ; Is PB6 by any chance HBL? 
-            lda #$00        ; timed interrupt
+            
+            ; D-VIA Aux control - $FFDB
+            ;     0------- T1 timer, PB7 disabled
+            ;     1x------ T1 timer, PB7 one-shot output (10) or square wave output (11)
+            ;     -0------ T1 timer, timed interrupt each time T1 is loaded (one-shot)
+            ;     -1------ T1 timer, continueous interrupts
+            ;     --0----- T2 timer, timed interrupt (1=count down with pulses on PB6)            
+            ;     ---000-- Shift Reg: disabled
+            ;     ---100-- Shift Reg: shift out free running at T2 rate
+            ;     ---1---- Shift Reg: shift out
+            ;     ---0---- Shift Reg: shift in
+            ;     ----01-- Shift Reg: under control of T2
+            ;     ----10-- Shift Reg: under control of O2
+            ;     ----11-- Shift Reg: under control of ext clock
+            ;     ------1- PB: Enable latching (0=disable)
+            ;     -------1 PA: Enable latching (0=disable)
+            ; D-VIA, no timers enabled            
+            lda #%00000000
             sta RD_AUXCTL
-            sta RE_AUXCTL
-            ; E-VIA - CA2 is keyboard, CA1 is clock; CB1, CB2 are VBL
-            ; CB2 - [hi nibble: 011-] independent interrupt input pos edge
-            ; CB1 - [hi nibble: ---0] neg active edge
-            ; CA2 - [lo nibble: 001-] independent interrupt input neg edge
-            ; CA1 - [lo nibble: ---0] neg active edge
-            lda #$62
-            sta RE_PERCTL
+            
             ; D-VIA - CA1 is any slot IRQ, CA2 is some switch?; CB1, CB2 are SCO/SER, probably joystick?
             ; CB2 - [hi nibble: 011-] independent interrupt input pos edge
             ; CB1 - [hi nibble: ---1] pos active edge
             ; CA2 - [lo nibble: 011-] independent interrupt input pos edge
             ; CA1 - [lo nibble: ---0] neg active edge
-            lda #$76
+            ; high nibble here is largely irrelevant, ineterrupts CB1, CB@ not used
+            ; maybe CB1, CB2 relate to joystick.
+            lda #%01110110
             sta RD_PERCTL
-            ; E-VIA Int Enable
-            ; [1-------] enable
-            ; [-1------] timer 1
-            ; [--1-----] timer 2
-            ; [---1----] CB1
-            ; [----0---] CB2
-            ; [-----0--] shift register
-            ; [------0-] CA1
-            ; [-------1] CA2
-            lda #$F1        ; enable timer1, timer2, CB1, CA2
+            
+            ; disable & clear certain E-VIA interrupts (MSB=clear, other bits=interrupts)
+            ; not sure why only some are disabled and cleared, this is based on Atomic Defense
+            ; E-VIA Int -  flag $FFED, enable $FFEE
+            ;     0------- disable
+            ;     -0------ timer 1
+            ;     --0----- timer 2
+            ;     ---0---- CB1 (VBL)
+            ;     ----1--- CB2 (VBL)
+            ;     -----1-- shift register (VBL)
+            ;     ------1- CA1 (RTC)
+            ;     -------0 CA2 (keyboard)
+            lda #%00001110        ; disable & clear CB2, shift register, CA1
             sta RE_INTENAB
-            ; E-VIA Int Flag
-            ; [0-------] no function I believe?
-            ; [-1------] timer 1
-            ; [--1-----] timer 2
-            ; [---1----] CB1
-            ; [----0---] CB2
-            ; [-----0--] shift register
-            ; [------0-] CA1
-            ; [-------1] CA2
-            lda #$71        ; clear timer1, timer2, CB1, CA2
             sta RE_INTFLAG
+            
+            ; E-VIA Aux control - $FFEB
+            ; [0-------] T1 timer, PB7 disabled
+            ; [1x------] T1 timer, PB7 one-shot output (10) or square wave output (11)
+            ; [-0------] T1 timer, timed interrupt each time T1 is loaded (one-shot)
+            ; [-1------] T1 timer, continueous interrupts
+            ; [--0-----] T2 timer, timed interrupt (1=count down with pulses on PB6)            
+            ; [---000--] Shift Reg: disabled
+            ; [---100--] Shift Reg: shift out free running at T2 rate
+            ; [---1----] Shift Reg: shift out
+            ; [---0----] Shift Reg: shift in
+            ; [----01--] Shift Reg: under control of T2
+            ; [----10--] Shift Reg: under control of O2
+            ; [----11--] Shift Reg: under control of ext clock
+            ; [------1-] PB: Enable latching (0=disable)
+            ; [-------1] PA: Enable latching (0=disable)
+            ; 
+            ; T1 has two latches and a 16 bit counter.  Down to zero -> interrupt
+            ; one shot keeps counting, free-run resets and counts again
+            ; T2 can count PB6 negatives, or run in one shot mode.
+            ; Count: load number to count into T2, dec on pulses, interrupt at zero, counting continues
+            ; Is PB6 by any chance HBL? Seems likely.
+            ; E-VIA: enable timer 2, one-shot.  HBL.
+            lda #%00100000
+            sta RE_AUXCTL
+            
+            ; E-VIA - CA2 is keyboard, CA1 is clock; CB1, CB2 are VBL
+            ; CB2 011- hi nibble - independent interrupt input pos edge (VBL)
+            ; CB1 ---0 hi nibble - neg active edge (VBL)
+            ; CA2     001- lo nibble independent interrupt input neg edge (keyboard)
+            ; CA1     ---0 lo nibble neg active edge (clock)
+            lda #%01100010
+            sta RE_PERCTL
+            
+            ; E-VIA Int Enable
+            ;     1------- enable
+            ;     -0------ timer 1
+            ;     --1----- timer 2 (I/O count, HBL)
+            ;     ---1---- CB1 (VBL)
+            ;     ----0--- CB2 (VBL)
+            ;     -----0-- shift register (VBL)
+            ;     ------0- CA1 (clock)
+            ;     -------1 CA2 (keyboard)
+            ; E-VIA - enable timer2 (HBL), CB1 (VBL), CA2 (keyboard)
+            lda #%10110001
+            sta RE_INTENAB
+            
+            ; E-VIA Int Flag
+            ;     0------- no function I believe?
+            ;     -0------ timer 1 
+            ;     --1----- timer 2 (I/O count, HBL)
+            ;     ---1---- CB1 (VBL)
+            ;     ----0--- CB2 (VBL)
+            ;     -----0-- shift register (VBL)
+            ;     ------0- CA1 (clock)
+            ;     -------1 CA2 (keyboard)
+            ; clear timer2, CB1, CA2
+            lda #%00110001
+            sta RE_INTFLAG
+            
+            ; set the HBL (E-VIA timer 2) going, kind of guarantees a fast interrupt
+            lda #$08
+            sta RE_T2CL     ;get set
+            lda #$00
+            sta RE_T2CH     ;go
             rts
 
 ; draw a single number
