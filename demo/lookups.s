f796ce58f10ee2063c09bc9c60241bbd360cf5d8
Paul Hagstrom <hagstrom@bu.edu>
2022-06-25 13:28:06 +0000

    Actually put in the zero-based lores values

--- demo/lookups.s
+++ demo/lookups.s
@@ -146,9 +146,9 @@ YLoresHB:
 			.byte $08, $08, $09, $09, $0A, $0A, $0B, $0B
 
 YLoresHZA:
-			.byte $04, $04, $05, $05, $06, $06, $07, $07
-			.byte $04, $04, $05, $05, $06, $06, $07, $07
-			.byte $04, $04, $05, $05, $06, $06, $07, $07
+			.byte $00, $00, $01, $01, $02, $02, $03, $03
+			.byte $00, $00, $01, $01, $02, $02, $03, $03
+			.byte $00, $00, $01, $01, $02, $02, $03, $03
 
 ; I took DOOM's "random" numbers.
 ; https://github.com/id-Software/DOOM/blob/77735c3ff0772609e9c8d29e3ce2ab42ff54d20b/linuxdoom-1.10/m_random.c#L27
